43172619dc5a00e47779c111b610802efa1811c1
Mels Ramblings <mfelton@mail.greenriver.edu>
2018-01-29 17:34:30 -0500

    Added ability to view recommended toys, from magic array

--- index.php
+++ index.php
@@ -11,14 +11,22 @@ $f3->set("DEBUG",3);
 $f3->route('GET /', function() {
     $template = new Template;
     echo $template->render
-    ('pages/home.html');
+    ('views/home.html');
 }
 );
 
-$f3->route('GET /recommend', function() {
+$f3->route('GET /recommend', function($f3) {
+    $f3->set("basicObjectType","Toy");
+//    $f3->set("Toys",array(array("Tear-apart Dog","Cat can tear apart dog and then you velcro him back together",5),
+//        array("Tear-apart Doggy","Cat can tear apart dog and then you velcro him back together",5),
+//        array("Long Dancing Fingers","Cat attacks tips of 'fingers' rather than yours.",6),
+//        array("Catnip Ball","Soft ball filled with catnip your cat will chase until he drops",2),
+//        array("Add A Toy Toy","Does your cat like something else? Add it here.",0)));
+    $f3->set("Toys",array("Tear-apart Dog"=>5,"Tear-apart Doggy"=>6,"Long Dancing Fingers"=>10
+            ,"Catnip Ball"=>3,"Add A Toy Toy"=>0));
     $template = new Template;
     echo $template->render
-    ('pages/rank.html');
+    ('views/rank.html');
 }
 );
 
