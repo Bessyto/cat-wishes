027143bf41ece6d304a0dc9a249ee98f7ef1a47a
btorres-miller.greenriverdev.com <btorresm@srv603.webhostingforstudents.com>
2018-02-25 18:11:18 -0500

    some changes

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ error_reporting(E_ALL);
 require_once('vendor/autoload.php');
 session_start();
 
-require_once 'model/db-functions.php';
+//require_once 'model/db-functions.php';
 
 $f3 = Base::instance();
 $f3->set("DEBUG", 3);
 
 //Connect to the database
-$dbh = connect();
+//$dbh = connect();
 
 $f3->set("Carousel", array("Toys" => "images/carouselToys.jpg", "Food" => "images/carouselFood.jpg",
     "Furniture" => "images/carouselFurniture.jpg", "Vets" => "images/carouselVet.jpg"));
