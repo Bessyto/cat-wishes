5b82411440d3132de2f69f3a0ec63310f2b2084a
Mels Ramblings <mfelton@mail.greenriver.edu>
2018-03-08 15:30:09 -0500

    Changed header to work for items pages

--- index.php
+++ index.php
@@ -12,6 +12,17 @@ $f3->set("DEBUG", 3);
 
 //Connect to the database
 $dbh = connect();
+/*
+ * Maybe this will work to kill that annowing session!
+ * <?php
+    session_start();
+    session_unset();
+    session_destroy();
+    session_write_close();
+    setcookie(session_name(),'',0,'/');
+    session_regenerate_id(true);
+?>
+ */
 require('userLogin.php');
 
 $f3->set("Carousel", array("Toys" => "images/carouselToys.jpg", "Food" => "images/carouselFood.jpg",
