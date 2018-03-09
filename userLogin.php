35a5318bd265390e01f62d8e5ecd6ef62819b1e1
Mels Ramblings <mfelton@mail.greenriver.edu>
2018-03-08 19:49:15 -0500

    Logout working

--- userLogin.php
+++ userLogin.php
@@ -3,47 +3,35 @@
 //    var_dump($_POST);
 //    echo "</pre>";
 
-    if (!empty($_POST['username'] && $_POST['password'])) {
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
 
-        $username = (empty($_POST['username'])) ? 'Something Went Wrong' : $_POST['username'];
-        $password = (empty($_POST['password'])) ? '' : $_POST['password'];
-        $table = "cat_members";
+    $username = (empty($_POST['username'])) ? 'Something Went Wrong' : $_POST['username'];
+    $password = (empty($_POST['password'])) ? '' : $_POST['password'];
+    $table = "cat_members";
 
-        if (checkMember($table, $username, $password))
-        {
-            //passing username to session
-            $_SESSION['username']=$username;
-        }
-        else{
-//
-            session_destroy();
-
-        }
-
+    if (checkMember($table, $username, $password)) {
+        //passing username to session
+        $_SESSION['username'] = $username;
     }
 
-    if (isset($_POST['logout']))
-    {
+
+}
+
+if (isset($_POST['logout'])) {
 //        unset($_SESSION['username']);
 //        $_POST['username'] ='';
 //        session_destroy();
 
+    session_unset();
+//    $f3->clear('SESSION');
+    session_destroy();
+    session_write_close();
+    setcookie(session_name(), '', 0, '/');
+//    session_regenerate_id(true);
 
-                session_unset();
-                $f3->clear('SESSION');
-                session_destroy();
-                session_write_close();
-                setcookie(session_name(),'',0,'/');
-                session_regenerate_id(true);
+//    $f3->reroute('./{{@item}}');
 
-
-
-
-        $f3->reroute('http://www.amazon.com');
-
-    }
-
-
+}
 
 
 ?>
