7fc2eee396e9693991878201172befb5a95aa116
btorres-miller.greenriverdev.com <btorresm@srv603.webhostingforstudents.com>
2018-03-18 00:49:08 -0400

    Cleaning code

--- classes/vets.php
+++ classes/vets.php
@@ -7,79 +7,23 @@
 /**
  * Class Vets represents a vet object
  * @author Bessy Torres-Miller
+ * @author Melanie Felton
  * @copyright 2017
  */
 class Vets extends BasicObject
 {
-    private $_address;
-    private $_link;
-    private $_phone;
-
-    /**
+     /**
      * Vets constructor.
      * @param $id
      * @param string $name
+     * @param string $description
      * @param int $recommendations
-     * @param $address
-     * @param $link
-     * @param $phone
+     *
      */
-    public function __construct($id, $name = "Generic Vet", $recommendations = 0, $address = "", $link = "", $phone = "")
+    public function __construct($id, $name = "Generic Vet", $description = "Does nothing." , $recommendations = 0)
     {
-        Parent::__construct($id, $name, $address, $recommendations);
-        $this->_address = $address;
-        $this->_link = $link;
-        $this->_phone = $phone;
+        Parent::__construct($id, $name, $description, $recommendations);
 
     }
-
-    /**
-     * @return mixed
-     */
-    public function getAddress()
-    {
-        return $this->_address;
-    }
-
-    /**
-     * @param mixed $address
-     */
-    public function setAddress($address)
-    {
-        $this->_address = $address;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getLink()
-    {
-        return $this->_link;
-    }
-
-    /**
-     * @param mixed $link
-     */
-    public function setLink($link)
-    {
-        $this->_link = $link;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPhone()
-    {
-        return $this->_phone;
-    }
-
-    /**
-     * @param mixed $phone
-     */
-    public function setPhone($phone)
-    {
-        $this->_phone = $phone;
-    }
-
 
 }
