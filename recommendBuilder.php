18c5bed9e7cfce3404625c13f20ae336eaa9c9c3
btorres-miller.greenriverdev.com <btorresm@srv603.webhostingforstudents.com>
2018-03-18 22:31:11 -0400

    headers added

--- recommendBuilder.php
+++ recommendBuilder.php
@@ -1,4 +1,17 @@
 <?php
+/*
+ *  recommendBuilder.php
+ *  Cat-Wishes Final Project
+ *  IT-328
+ *  Melanie Felton
+ *  Bessy Torres-Miller
+ *
+ *  This file grabs the information from the table when the table name passed as item and prompts
+ *  the user with the items in that table ( rendering to rank.html)
+ *  The delete option will be shown to users with access level 2.
+ *  Some validation is done for the item name and description enter by the user.
+ *
+ */
 
 $basicObjectType = $params['item'];
 $accessLevel = $_SESSION['access'];
